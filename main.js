80e0ee0b2db2e0dbfe96fdd821d5c23b6d1b7ba8
cartrugs <cartrugs@gmail.com>
2023-06-22 11:07:45 +0200

    Ejercicio 2

---
+++ main.js
@@ -0,0 +1,29 @@
+//Ejercicio 1
+
+const contarVeces = (sentence, letra) => {
+    let contadorVeces = 0;
+
+    for (let i=0; i<sentence.length; i++) {
+        if (sentence[i] === letra) {
+            contadorVeces++
+        }
+    }
+
+    return contadorVeces;
+}
+
+//Ejercicio 2
+
+const imprimirSiguientesCincuenta = () => {
+    let numUsuario = parseInt(prompt('Ingresa un número:'));
+    let imparesAcumulados = [];
+    let sigImpar = 0;
+
+    while (numUsuario < 50) {
+        if (numUsuario % 2 !==0) {
+            imparesAcumulados++
+            console.log(sigImpar)
+            sigImpar++
+        }  
+    }
+};
